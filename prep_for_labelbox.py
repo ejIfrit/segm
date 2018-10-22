e51a03d63687a9c42bfd9c4688ea7ac0bc618477
ejIfrit <edward.jackson815@gmail.com>
2018-10-22 23:03:00 +0800

    code for zooming images for labelbox submission, and getting images from labelbox for training

--- prep_for_labelbox.py
+++ prep_for_labelbox.py
@@ -3,38 +3,46 @@
 Look at a file and see if a mask exists for it. Analyse the image based on the mask
 
 Usage:
-    test.py (prep) [--fileName=<fn>] [--nCrop=<n1>] [--nTubs=<nt>]
+    test.py (prep) [--path=<fn>] [--nResize=<n1>] [--nTubs=<nt>]
 
 Options:
     -h --help        Show this screen.
 """
-
-
-def prep_for_labelBox(path,nResize,tubs='all'):
+import os
+import sys
+from docopt import docopt
+import glob
+import ast
+import numpy as np
+import cv2
+def prep_for_labelBox(path,nResize,tubs='all',dirToSave = 'forLabelling'):
     print(path)
     print(tubs)
     print(nResize)
-    pass
+    tubsList = []
+    if tubs!='all':
+        tubsList = eval('np.r_'+tubs)
+        print(tubsList)
+        #print(fn[len(path)+1:])
+    if not os.path.exists(path+'/'+dirToSave):
+        os.makedirs(path+'/'+dirToSave)
 
+    for fn in glob.glob(path+'/'+'*.jpg'):
+            tubNo = int("".join(filter(str.isdigit, fn[len(path)+1:])))
+            if tubs!='all' and tubNo in tubsList:
+                print(tubNo)
+                img1_orig = cv2.imread((fn))
+                img1_out =img1_orig#= cv2.cvtColor(img1_orig, cv2.COLOR_BGR2RGB)
+                if nResize!=1:
+                    height,width,depth = img1_out.shape
+                    img1_out = cv2.resize(img1_out,(int(width*nResize),int(height*nResize)))
+                    cv2.imwrite(os.path.join(path+'/'+dirToSave , str(tubNo)+'resized'+'.jpg'), img1_out)
 
 
 if __name__ == '__main__':
     args = docopt(__doc__)
     print(args)
-    cd = True if args['--cd'] else False
-    hsv = True if args['--hsv'] else False
-    if args['--nCrop']:
-        tubs =
-    else:
-        nc = 0
-    if args['--fileName']:
-        analyseMasks(args['--fileName'],isCone = cd)
-    if args['--fileName'] and args['--nCrop']:
-        analyseMasks(args['--fileName'],int(args['--nCrop']),isCone = cd)
-    if args['folder']:# and args['--folderName']:
-        analyseFolder(isCone = cd, doHSV = hsv)
-    if args['clf'] and args['--test']:
-        testClfMask(isCone=cd,nCrop=nc)
-    if args['clf'] and not args['--test']:# and args['--folderName']:
-        sm = True if args['--s'] else False
-        doClf(isCone = cd, doHSV = hsv,saveModel = sm)
+
+    tubsIn = args['--nTubs'] if args['--nTubs'] else 'all'
+    if args['--path'] and args['--nResize'] and args['--nTubs']:
+        prep_for_labelBox(args['--path'],nResize = int(args['--nResize']),tubs=tubsIn)
---
+++ read_from_labelbox.py
@@ -0,0 +1,63 @@
+import sys
+import json
+import glob
+from skimage import io
+import matplotlib as mpl
+import cv2
+import numpy as np
+mpl.use('TkAgg')
+import matplotlib.pyplot as plt
+def getMaskFromLabelBox(export_folder,labelKey='Track',nResize=1):
+    #export_folder = sys.argv[1]
+    export_file = glob.glob(export_folder+'/'+'*.json')
+    with open(export_file[0]) as f:
+        loaded_json = json.load(f)
+        ims = []
+        masks = []
+        for x in loaded_json:
+            print(x)
+            url = x['Label']['segmentationMasksByName']['Track']
+            print(url)
+            mask = io.imread(url)
+            mask = mask[:,:,:]
+            gray = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+            ret,thresh1 = cv2.threshold(gray,1,255,cv2.THRESH_BINARY)
+            #plt.imshow(thresh1)
+            url = x['Labeled Data']
+            imOut = io.imread(url)
+            #plt.imshow(thresh1)
+            if nResize!=1:
+                height,width,depth = img1_orig.shape
+                imOut = cv2.resize(imOut,(int(width*nResize),int(height*nResize)))
+                thresh1 = cv2.resize(thresh1,(int(width*nResize),int(height*nResize)))
+
+            ims.append(imOut)
+            masks.append(thresh1)
+        ims = np.array(ims)
+        masks = np.array(masks)
+        print(ims.shape)
+        print(masks.shape)
+        return ims,masks
+            #plt.imshow(imOut)
+            #plt.show()
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        fail_for_missing_file()
+
+    export_folder = sys.argv[1]
+    export_file = glob.glob(export_folder+'/'+'*.json')
+    getMaskFromLabelBox(export_folder)
+    #with open(export_file[0]) as f:
+
+    #    loaded_json = load(f)
+    #    for x in loaded_json:
+        	#print(x)
+
+            #url = x['Label']['segmentationMaskURL']
+    #        url = x['Label']['segmentationMasksByName']['Track']
+    #        print(url)
+    #        image = io.imread(url)
+    #        plt.imshow(image)
+    #        plt.show()
+            #print(loaded_json[x])
